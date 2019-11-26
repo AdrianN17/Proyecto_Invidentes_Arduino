ff737069ed4b66bc24333168a9e599ff1ad7ade0
Adrian <36133718+AdrianN17@users.noreply.github.com>
2019-11-26 03:31:25 -0600

    Mejora del codigo
    Agregado modulo de  gps
    Experimental

--- sketch_oct11a/sketch_oct11a.ino
+++ sketch_oct11a/sketch_oct11a.ino
@@ -1,48 +1,78 @@
-#include <SoftwareSerial.h>
+#include "TinyGPS++.h"
+#include "AltSoftSerial.h"
+#include "NeoSWSerial.h"
 
-#define PIN_TRIGGER 7
-#define PIN_ECHO 8
-#define PIN_LED 4
 
-SoftwareSerial BT(10, 11); // RX | TX
+#define PIN_TRIGGER 6
+#define PIN_ECHO 7
 
-void setup() {
+//NeoSWSerial BT(8 ,9); // RX | TX
+NeoSWSerial serial_connection(2,3); //RX=pin 10, TX=pin 11
+TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
 
+float counter = 0;
+float max_counter = 1.5;
+float oldTime = 0;
+
+
+float datos[3] = {0,0,0};
+
+void setup()
+{
   pinMode(PIN_TRIGGER, OUTPUT);
   pinMode(PIN_ECHO, INPUT); 
   digitalWrite(PIN_TRIGGER, LOW);
+ 
+  serial_connection.begin(9600);
 
-  pinMode(PIN_LED,INPUT);
-  
-  BT.begin(9600);
-  Serial.begin(9600);
-
+  //BT.begin(9600);
 }
 
-void loop() {
-  digitalWrite(PIN_LED, HIGH);
-
+void loop()
+{
+  gps_modulo();
   
-  long t; 
-  long d; 
+  ultrasonido_modulo();
+  
+  String cadena=  (String)datos[0] + "-" + (String)datos[1] + "-" +(String)datos[2] ;
+  
+  //BT.println(cadena);
+   
+}
+
+void ultrasonido_modulo()
+{
+
+  long t;  
 
   digitalWrite(PIN_TRIGGER, HIGH);
   delayMicroseconds(10);       
   digitalWrite(PIN_TRIGGER, LOW);
   
   t = pulseIn(PIN_ECHO, HIGH);
-  d = t/59; 
-  
-  /*Serial.print("Distancia: ");
-        
-  Serial.print("cm");
-  Serial.println();*/
-  BT.println(d);
+  datos[0] = t/59; 
 
-  
-  
-  delay(100);      
+}
 
-     
+void gps_modulo()
+{
+  while(serial_connection.available())//While there are characters to come from the GPS
+  {
+      gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
+  }
+  
+  if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
+  {    
+    datos[1] = gps.location.lat();
+    datos[2] = gps.location.lng();
+  }
+}
 
+void changed(bool value)
+{
+  if(value)
+  {
+
+    
+  }
 }
