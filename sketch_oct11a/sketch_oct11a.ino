f3fede334102a694a1feb67a323470d35f34004f
Adrian <36133718+AdrianN17@users.noreply.github.com>
2019-11-29 00:18:51 -0600

    Mejora de envio
    Ahora envia usando ambos sensores, gps y bluetooth

--- sketch_oct11a/sketch_oct11a.ino
+++ sketch_oct11a/sketch_oct11a.ino
@@ -1,40 +1,67 @@
-#include "TinyGPS++.h"
-#include "NeoSWSerial.h"
-#include "AltSoftSerial.h"
+#include <NMEAGPS.h>
+const float FEET_PER_METER = 3.28084;
+
+#include <AltSoftSerial.h>// import the serial library
+AltSoftSerial gpsPort; // RX is 8 (to GPS tx), TX is 9 (to GPS rx)
+
+#define Genotronex Serial  // just an "alias" for Serial to make the code more readable
+
+NMEAGPS gps; // the parser
+gps_fix fix; // the struct with all the parsed values
+
 
 #define PIN_TRIGGER 6
 #define PIN_ECHO 7
 
-NeoSWSerial BT(10,11); // RX | TX
-NeoSWSerial serial_connection(9,8); //RX=pin 10, TX=pin 11
-TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
-
-
 float datos[3] = {0,0,0};
 
-void setup()
-{
-  Serial.begin(9600);
-  
-  //BT.begin(9600);
 
-  serial_connection.begin(9600);
-  
+void setup() {
+  //Serial.begin(9600);  // not needed if Genotronex *is* Serial
+  gpsPort.begin(9600);
+  Genotronex.begin(9600);
+  Genotronex.println("GPS Start");
+  Genotronex.println("Bluetooth On please press 1 or 0 blink LED ..");
+
   pinMode(PIN_TRIGGER, OUTPUT);
   pinMode(PIN_ECHO, INPUT); 
   digitalWrite(PIN_TRIGGER, LOW);
+
 }
 
-void loop()
-{
-  gps_modulo();
+void loop() {
+
+  if (gps.available( gpsPort)) {
+    fix = gps.read();  // get the latest GPS info (all in one struct)
+
+    datos[1] = fix.latitude();
+    datos[2] = fix.longitude();
   
+
+
+      /*Genotronex.println("LED  On D13 ON ! ");
+
+      //Get the latest info from the gps object which it derived from the data sent by the GPS unit
+      Genotronex.println("Satellite Count:");
+      Genotronex.println( fix.satellites );
+      Genotronex.println("Latitude:");
+      Genotronex.println( fix.latitude(), 6);
+      Genotronex.println("Longitude:");
+      Genotronex.println( fix.longitude(), 6);
+      Genotronex.println("Speed MPH:");
+      Genotronex.println( fix.speed_mph() );
+      Genotronex.println("Altitude Feet:");
+      Genotronex.println( fix.altitude() * FEET_PER_METER );
+      Genotronex.println();*/
+ 
+    
+  }
   ultrasonido_modulo();
   
   String cadena=  (String)datos[0] + "-" + (String)datos[1] + "-" +(String)datos[2] ;
   
-  //BT.println(cadena);
-  Serial.println(cadena);
+  Genotronex.println(cadena);
+  
 }
 
 void ultrasonido_modulo()
@@ -49,18 +76,4 @@ void ultrasonido_modulo()
   t = pulseIn(PIN_ECHO, HIGH);
   datos[0] = t/59; 
 
-}
-
-void gps_modulo()
-{
-  while(serial_connection.available())//While there are characters to come from the GPS
-  {
-      gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
-  }
-  
-  if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
-  {    
-    datos[1] = gps.location.lat();
-    datos[2] = gps.location.lng();
-  }
 }
